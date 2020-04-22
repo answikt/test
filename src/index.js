bc3fc6d260104383ad9289f855f68e0e2371adfe
ansvikt <ansvikt@mail.ru>
2020-04-22 12:36:39 +0300

    Del index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import store from "./store";
-import './index.css';
+import './styles.scss';
 import App from './containers/app';
 
 ReactDOM.render(
